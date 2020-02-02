f55665a1c137402996049543ade901b61ab1c267
Adam Sisk <adamsisk91@gmail.com>
2020-02-02 01:26:22 -0600

    👀 nav

--- src/components/layout/Nav.js
+++ src/components/layout/Nav.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import styled from 'styled-components';
 
+/**
+ * Nav styles
+ */
 const NavContainer = styled.nav`
   position: sticky;
   height: 60px;
   left: 0px;
   top: 0px;
-  background-color: ${props => props.theme.blue};
+  /* background-color: ${props => props.theme.blue}; */
+  background-image: ${props => {
+    const { red, orange, yellow, green, blue } = props.theme;
+    return `linear-gradient(
+      to right,
+      ${blue} calc(80vw - 50px), ${green}, ${yellow}, ${orange}, ${red} 105%
+    )`;
+  }};
+  /* border: 2px solid ${props => props.theme.blue}; */
+  border-bottom-left-radius: 7px;
+  border-bottom-right-radius: 7px;
   display: grid;
   align-items: center;
   ul {
@@ -26,7 +39,7 @@ const NavContainer = styled.nav`
   }
 `;
 
-function Nav(props) {
+function Nav() {
   return (
     <NavContainer>
       <ul>
